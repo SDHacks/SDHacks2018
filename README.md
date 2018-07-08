856f36854575e07b7236434e27f31756cbb1909d
Subhankar Panda <s1panda@ucsd.edu>
2018-07-08 13:03:49 -0400

    Added Navbar And Footer, Dev Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# SD Hacks 2018
+
+Official Website for SD Hacks, UC San Diego's annual collegiate hackathon
+
+Build with ```gulp```
+
+Contact <hello@sdhacks.io> for any questions.
---
+++ js/sdhacks.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    let _nav = $('#top-nav');
+    const FADE_DURATION = 200, 
+        SCROLL_TRESHOLD = 100;
+
+    _nav.hide();
+
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > SCROLL_TRESHOLD) { 
+                _nav.fadeIn(FADE_DURATION);
+            } else {
+                _nav.fadeOut(FADE_DURATION);
+            }
+        });
+    });
+
+});
